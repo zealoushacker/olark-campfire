07a1d802af617dd22b434ed19e05571c4764d0ad
alexnotov <anotov@gmail.com>
2011-10-03 10:35:59 -0700

    Rethinking design. Started writing spec for olark interface

--- app.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'blather/client'
-
-client=setup ENV['JID'], ENV['JPASSWORD'], ENV['JHOST'], ENV['JPORT']  
-
-
---
+++ spec/olark_interface_spec.rb
@@ -0,0 +1,11 @@
+require './lib/olark_interface'
+
+describe OlarkInterface do
+  it "should listen for messages from oLark"
+
+  it "should place messages from oLark onto a Starling chat message queue"
+
+  it "should monitor Starling chat message queue for replies to messages"
+
+  it "should send replies to messages to the user for whom the message was created"
+end
